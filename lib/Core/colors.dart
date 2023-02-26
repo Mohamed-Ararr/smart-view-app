4642c2e87e48ad1916c641b7116f0b2233f63e74
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-02-26 15:36:28 +0000

    add needed colors

---
+++ lib/Core/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color mainColor = const Color(0xff010101);
+  static Color cardColor = const Color(0xff2F3034);
+  static Color secondColor = const Color(0xFFD9D9D9);
+}
