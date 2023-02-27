f87d0f138533c1076bc3a8bc696917078607f7d6
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-02-27 15:39:40 +0000

    add phone screen app bar

---
+++ lib/Features/PhoneSpecsScreen/Presentation/Widgets/phoneAppBar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+
+class PhoneAppBar extends StatelessWidget {
+  const PhoneAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          onPressed: () => GoRouter.of(context).pop(),
+          icon: const Icon(
+            FontAwesomeIcons.circleArrowLeft,
+            size: 30,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            FontAwesomeIcons.heart,
+            size: 30,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/Features/PhoneSpecsScreen/Presentation/Widgets/phoneSpecsScreenBody.dart
+++ lib/Features/PhoneSpecsScreen/Presentation/Widgets/phoneSpecsScreenBody.dart
@@ -1,10 +1,27 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
+import 'package:smartviewapp/Features/PhoneSpecsScreen/Presentation/Widgets/phoneAppBar.dart';
+import 'package:smartviewapp/constants.dart';
 
 class PhoneSpecsScreenBody extends StatelessWidget {
   const PhoneSpecsScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: kPaddingRL,
+          child: SingleChildScrollView(
+            child: Column(
+              children: const [
+                PhoneAppBar(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
