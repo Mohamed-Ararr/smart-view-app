569e217cfeb92f049dc9b029293465e38bdcf35f
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-02-27 15:23:10 +0000

    build bottom nav bar

--- lib/Features/homeScreen/Presentation/Widgets/homeScreenBody.dart
+++ lib/Features/homeScreen/Presentation/Widgets/homeScreenBody.dart
@@ -1,20 +1,41 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:smartviewapp/Core/bottomNav.dart';
 import 'package:smartviewapp/Core/colors.dart';
-import 'package:smartviewapp/Core/fontOpt.dart';
-import 'package:smartviewapp/Features/homeScreen/Presentation/Widgets/exploreCategories.dart';
-import 'package:smartviewapp/Features/homeScreen/Presentation/Widgets/homeAppBar.dart';
-import 'package:smartviewapp/constants.dart';
 
+import '../../../../constants.dart';
+import 'exploreCategories.dart';
 import 'exploreProducts.dart';
+import 'homeAppBar.dart';
 
-class HomeScreenBody extends StatelessWidget {
+class HomeScreenBody extends StatefulWidget {
   const HomeScreenBody({super.key});
+
+  @override
+  State<HomeScreenBody> createState() => _HomeScreenBodyState();
+}
+
+class _HomeScreenBodyState extends State<HomeScreenBody> {
+  List<Widget> screens = const [
+    HomeScreenWidget(),
+    FavoriteScreenWidget(),
+  ];
+
+  int selectedScreen = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNav(
+        onTapChange: (index) {
+          setState(() {
+            selectedScreen = index;
+          });
+        },
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
@@ -22,17 +43,48 @@ class HomeScreenBody extends StatelessWidget {
             padding: kPaddingRL,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                HomeAppBar(),
-                SizedBox(height: 20),
-                ExploreCategories(),
-                SizedBox(height: 30),
-                ExploreProducts(),
+              children: [
+                const HomeAppBar(),
+                const SizedBox(height: 20),
+                screens.elementAt(selectedScreen),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class FavoriteScreenWidget extends StatelessWidget {
+  const FavoriteScreenWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text("hw arrqdf"),
+      ],
+    );
+  }
+}
+
+class HomeScreenWidget extends StatelessWidget {
+  const HomeScreenWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        ExploreCategories(),
+        SizedBox(height: 30),
+        ExploreProducts(),
+      ],
     );
   }
 }
