ac6d13f1b8b40774d4513e8b6fba52fe17c79001
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-04 15:01:44 +0000

    create fetchphone method

--- lib/Core/apiService.dart
+++ lib/Core/apiService.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  final String baseUrl = "https://api.device-specs.io/api/";
-  final Dio dio;
+  final String _baseUrl = "https://api.device-specs.io/api/";
+  final Dio _dio;
 
-  ApiService(this.dio);
+  ApiService(this._dio);
 
   Future<Map<String, dynamic>> get({required String endPoint}) async {
     Options opt = Options(
@@ -13,7 +13,7 @@ class ApiService {
             "Bearer 714cb064db14b71aa18deb5c9e48725b806be0aac31ecfc6c42c8a9843d0f6f358e9880a8042247c5b71caa334758101cd2102cc44f9e66f6b23fc362e828bf29e0f8197aab49a7a5db8c51f2a114b6b04890e543051b4f50c49ca39311a306e4d5172c654c167e8b5ad3905aaf77e9c4d653d419bf13d2b42b4f428fe1c81b7",
       },
     );
-    Response response = await dio.get(baseUrl, options: opt);
+    Response response = await _dio.get("$_baseUrl$endPoint", options: opt);
     return response.data;
   }
 }
--- lib/Features/homeScreen/Data/Repos/home_repo_impl.dart
+++ lib/Features/homeScreen/Data/Repos/home_repo_impl.dart
@@ -1,17 +1,32 @@
 import 'package:dio/dio.dart';
+import 'package:smartviewapp/Core/apiService.dart';
 import 'package:smartviewapp/Features/homeScreen/Data/Models/phone_model/phone_model.dart';
 import 'package:smartviewapp/Core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:smartviewapp/Features/homeScreen/Data/Repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<PhoneModel>>> fetchCategories() async {
-    throw UnimplementedError();
+  Future<Either<Failure, List<PhoneModel>>> fetchPhones() async {
+    try {
+      var data = await apiService.get(endPoint: "smartphones?populate=*");
+      List<PhoneModel> phoneList = [];
+
+      for (var item in data["data"]) {
+        phoneList.add(PhoneModel.fromJson(item));
+      }
+
+      return right(phoneList);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<PhoneModel>>> fetchPhones() {
+  Future<Either<Failure, List<PhoneModel>>> fetchCategories() {
     // TODO: implement fetchPhones
     throw UnimplementedError();
   }
