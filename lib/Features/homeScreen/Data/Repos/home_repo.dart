37d727df470f4fa080475c4c93c7abec64d07159
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-04 14:28:14 +0000

    add home repos

---
+++ lib/Features/homeScreen/Data/Repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:smartviewapp/Core/errors/failure.dart';
+import 'package:smartviewapp/Features/homeScreen/Data/Models/phone_model/phone_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<PhoneModel>>> fetchPhones();
+  Future<Either<Failure, List<PhoneModel>>> fetchCategories();
+}
